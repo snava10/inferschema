664c0df77feeaa46334268195514d06003e13e45
snava10 <sergio.nava89@gmail.com>
2020-07-16 12:50:54 +0100

    using absolute paths. renaming main function to infer

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,9 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
+  moduleDirectories: ["node_modules", "src", "test"],
+  moduleNameMapper: {
+    "src/(.*)": "<rootDir>/src/$1",
+    "test/(.*)": "<rootDir>/test/$1",
+  },
 };
